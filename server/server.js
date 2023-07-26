7e1a4947ed50a06209cb3f11d6ea7f9a9a949722
Upendra Kumar <toxic74412@gmail.com>
2023-07-26 22:53:47 +0530

    Changes database connection

--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 const port = process.env.PORT;
-const db = process.env.DATABASE;
+const db = process.env.DATABASE_URI.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(db)
